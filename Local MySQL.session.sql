1ffb1df01386ad742b12efc914c683e97ad1bc79
Alana <165056608+Kittykatz911@users.noreply.github.com>
2025-07-21 19:11:50 +0000

    UPDATE mysql

--- Local MySQL.session.sql
+++ Local MySQL.session.sql
@@ -14,7 +14,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(20) NOT NULL,
     email VARCHAR(50),
-    password VARCHAR(20) NOT NULL,
+    pass VARCHAR(20) NOT NULL,
     first_name VARCHAR(15) NOT NULL,
     last_name VARCHAR(15)
 );
