ed28d558f66bc89823afe8c7966d2bf7e1f1d180
Alana <165056608+Kittykatz911@users.noreply.github.com>
2025-07-21 18:52:28 +0000

    UPDATE MySQL

---
+++ Local MySQL.session.sql
@@ -0,0 +1,20 @@
+USE bookstore;
+
+CREATE TABLE books (
+    reference_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    published_year INT,
+    genre VARCHAR(25) NOT NULL,
+    price DECIMAL(6, 2) NOT NULL,
+    units INT NOT NULL
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(50),
+    password VARCHAR(20) NOT NULL,
+    first_name VARCHAR(15) NOT NULL,
+    last_name VARCHAR(15)
+);
